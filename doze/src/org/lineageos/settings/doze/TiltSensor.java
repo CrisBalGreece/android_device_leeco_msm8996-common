8184bc992da232d14f1c908915dadf6002aa9e6e
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:25:13 +0000

    msm8996-common: parts: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to preference screen
     * Remove onOptionsItemSelected and where necessary move to activity
    
    Change-Id: I87af15f5e165aa7bcd556eb2b716c0ce624ca4e7

--- doze/src/org/lineageos/settings/doze/TiltSensor.java
+++ doze/src/org/lineageos/settings/doze/TiltSensor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (C) 2015 The CyanogenMod Project
  *               2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -67,7 +67,7 @@ public class TiltSensor implements SensorEventListener {
         }
 
         if (event.values[0] == 1) {
-            Utils.launchDozePulse(mContext);
+            DozeUtils.launchDozePulse(mContext);
         }
     }
 
