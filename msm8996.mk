09f0d84e8ad4b54c086a9f94c788951bd40e76b8
CrisBal <crisbaladim@yahoo.gr>
2021-10-22 13:19:17 +0300

    msm 8996-common: Build fwk-detect.vendor
    * Because we updating to S ,these "libqti_vndfwk_detect*"  "libqti_vndfwk_detect..vendor*"are requied by audio/BT/CNE/GPS/IMS etc.

--- msm8996.mk
+++ msm8996.mk
@@ -203,7 +203,8 @@ PRODUCT_PACKAGES += \
 
 # fwk-detect
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
