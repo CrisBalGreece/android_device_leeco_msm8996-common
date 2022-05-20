8356f2e7e64b1883669354930992302615ff7fe1
CrisBal <crisbaladim@yahoo.gr>
2022-05-20 09:34:26 +0300

    msm8996-common: Nuke again power.stats@1.0-service.mock
    Doesn't workin on A12
    
    E android.hardware.power.stats@1.0-service-mock: Failed to getEnergyData
    Build android.hardware.power.stats@1.0.vendor

--- msm8996.mk
+++ msm8996.mk
@@ -361,7 +361,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
+    android.hardware.power.stats@1.0.vendor \
     vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
