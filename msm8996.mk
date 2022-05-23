6215e55079b647637a177b92809b9787d0471497
ZeeLog <Danjel.Zammit@gmail.com>
2022-05-23 07:38:38 +0300

    msm8996-common: proprietary-files.txt: Update DRM + Widevine
    *from google/coral/coral:12/SQ1A.220105.002/7961164:user/release-keys
    E QSEECOMD: : Init dlopen(libspl.so, RLTD_NOW) is failed.... dlopen failed: library libspl.so not found
    E QSEECOMD: : Init dlopen(libops.so, RLTD_NOW) is failed.... dlopen failed: library libops.so not found

--- msm8996.mk
+++ msm8996.mk
@@ -216,6 +216,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 # For config.fs
