6ec16412b71a6212a417776f2ec38013a53dc0f2
CrisBal <crisbaladim@yahoo.gr>
2022-05-31 14:34:07 +0300

    msm8996-common: Import qti power HAL
    * Rename it to android.hardware.power-service-x2
    * Fix utils.c for our 4core msm8996
    * Add power hal to file_contexts
    
    * Fixing following errors while Enforcing:
    
    I servicemanager: Since 'android.hardware.power.IPower/default' could not be found, trying to start it as a lazy AIDL service
    04-08 09:19:56.602
    W libc : Unable to set property "ctl.interface_start" to "aidl/android.hardware.power.IPower/default": error code: 0x20
    04-08 09:19:56.603
    I servicemanager: Tried to start aidl service android.hardware.power.IPower/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.

--- msm8996.mk
+++ msm8996.mk
@@ -368,10 +368,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service-x2 \
+    vendor.qti.hardware.perf@1.0 \
+    vendor.qti.hardware.perf@2.0 \
+    vendor.qti.hardware.perf@2.0.vendor \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0.vendor \
-    vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
