49abd457b96f0773775227801919ceb6c07876f9
CrisBalGreece <crisbaladim@yahoo.gr>
2022-12-29 10:01:02 +0200

    msm8996-common:Fix some build warnings and add new broken rules
    Remove also kernel flags. Included now inside kernel/makefile

--- msm8996.mk
+++ msm8996.mk
@@ -189,10 +189,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LeEcoSettings
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Display
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
