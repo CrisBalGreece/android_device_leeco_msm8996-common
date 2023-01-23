ca62998a8cafa5837594dc223219d2a61a29a799
CrisBal <crisbaladim@yahoo.gr>
2023-01-23 13:54:19 +0200

    msm8996-common: Add device_framework_compatibility_matrix.xml
    Satisfy compiling errors:
    The following HALs in device manifest are not declared in FCM <= level 3:
      android.hardware.audio.effect@5.0::IEffectsFactory/default
      android.hardware.audio@5.0::IDevicesFactory/default
      android.hardware.bluetooth.audio.IBluetoothAudioProviderFactory/default (@2)
      android.hardware.cas@1.2::IMediaCasService/default
      android.hardware.drm.IDrmFactory/clearkey (@1)
      android.hardware.drm@1.4::ICryptoFactory/clearkey
      android.hardware.drm@1.4::IDrmFactory/clearkey
      android.hardware.drm@1.4::IDrmFactory/widevine
      android.hardware.gnss@1.0::IGnss/gnss_vendor
      android.hardware.gnss@2.0::IGnss/default
      android.hardware.power.IPower/default (@1)
      android.hardware.radio.deprecated@1.0::IOemHook/slot1
      android.hardware.radio.deprecated@1.0::IOemHook/slot2
      android.hardware.radio@1.1::ISap/slot2
      android.hardware.wifi.hostapd.IHostapd/default (@1)
      android.hardware.wifi.supplicant.ISupplicant/default (@1)
      android.hardware.wifi@1.6::IWifi/default
      com.qualcomm.qti.dpm.api@1.0::IdpmQmi/dpmQmiService
      com.qualcomm.qti.imscmservice@1.0::IImsCmService/qti.ims.connectionmanagerservice
      com.qualcomm.qti.uceservice@1.0::IUceService/com.qualcomm.qti.uceservice
      vendor.display.color@1.0::IDisplayColor/default
      vendor.display.config@1.9::IDisplayConfig/default
      vendor.display.postproc@1.0::IDisplayPostproc/default
      vendor.lineage.touch@1.0::IKeyDisabler/default
      vendor.lineage.trust@1.0::IUsbRestrict/default
      vendor.qti.data.factory@2.0::IFactory/default
      vendor.qti.gnss@1.0::ILocHidlGnss/gnss_vendor
      vendor.qti.gnss@1.2::ILocHidlGnss/gnss_vendor
      vendor.qti.gnss@2.1::ILocHidlGnss/gnss_vendor
      vendor.qti.gnss@3.0::ILocHidlGnss/gnss_vendor
      vendor.qti.hardware.alarm@1.0::IAlarm/default
      vendor.qti.hardware.bluetooth_sar@1.0::IBluetoothSar/default
      vendor.qti.hardware.btconfigstore@1.0::IBTConfigStore/default
      vendor.qti.hardware.cryptfshw@1.0::ICryptfsHw/default
      vendor.qti.hardware.data.connection@1.0::IDataConnection/slot1
      vendor.qti.hardware.data.connection@1.0::IDataConnection/slot2
      vendor.qti.hardware.data.latency@1.0::ILinkLatency/default
      vendor.qti.hardware.fingerprint@1.0::IQtiExtendedFingerprint/default
      vendor.qti.hardware.perf@2.0::IPerf/default
      vendor.qti.hardware.qdutils_disp@1.0::IQdutilsDisp/default
      vendor.qti.hardware.radio.am@1.0::IQcRilAudio/slot1
      vendor.qti.hardware.radio.am@1.0::IQcRilAudio/slot2
      vendor.qti.hardware.radio.ims@1.0::IImsRadio/imsradio0
      vendor.qti.hardware.radio.ims@1.0::IImsRadio/imsradio1
      vendor.qti.hardware.radio.lpa@1.0::IUimLpa/UimLpa0
      vendor.qti.hardware.radio.lpa@1.0::IUimLpa/UimLpa1
      vendor.qti.hardware.radio.qcrilhook@1.0::IQtiOemHook/oemhook0
      vendor.qti.hardware.radio.qcrilhook@1.0::IQtiOemHook/oemhook1
      vendor.qti.hardware.radio.qtiradio@1.0::IQtiRadio/slot1
      vendor.qti.hardware.radio.qtiradio@1.0::IQtiRadio/slot2
      vendor.qti.hardware.radio.uim@1.0::IUim/Uim0
      vendor.qti.hardware.radio.uim@1.0::IUim/Uim1
      vendor.qti.hardware.radio.uim_remote_client@1.0::IUimRemoteServiceClient/uimRemoteClient0
      vendor.qti.hardware.radio.uim_remote_client@1.0::IUimRemoteServiceClient/uimRemoteClient1
      vendor.qti.hardware.radio.uim_remote_server@1.0::IUimRemoteServiceServer/uimRemoteServer0
      vendor.qti.hardware.radio.uim_remote_server@1.0::IUimRemoteServiceServer/uimRemoteServer1
      vendor.qti.imsrtpservice@1.0::IRTPService/imsrtpservice
    COMPATIBLE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,6 +134,8 @@ USE_DEVICE_SPECIFIC_GPS := true
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(PLATFORM_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(PLATFORM_PATH)/configs/device_framework_compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(PLATFORM_PATH):libinit_leeco_msm8996
