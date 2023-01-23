4b847f26c0b8c1dbf8b8cab2258f91e34f2520df
CrisBal <crisbaladim@yahoo.gr>
2023-01-23 12:36:29 +0200

    msm8996-common: Change libhidl shim to satisfy hardware/lineage/compat

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -15,7 +15,7 @@
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := libhidlbase
-LOCAL_MODULE := android.hidl.base@1.0
+LOCAL_MODULE := android.hidl.base@1.0_vendor
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true
@@ -23,7 +23,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := libhidlbase
-LOCAL_MODULE := android.hidl.manager@1.0
+LOCAL_MODULE := android.hidl.manager@1.0_vendor
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true
