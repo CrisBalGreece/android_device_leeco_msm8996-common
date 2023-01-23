372ca6399757321580afe491f8674b21ac47028a
hpnightowl <iamhp2k@gmail.com>
2023-01-23 12:25:01 +0200

    msm8996-common:Drop audio.a2dp.default
    * Building from here:
       https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35

--- msm8996.mk
+++ msm8996.mk
@@ -91,7 +91,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
