6b62b52112458edc09a341788ac5aff560776f3d
CrisBal <crisbaladim@yahoo.gr>
2023-01-28 15:13:16 +0200

    Revert "msm8996-common:Migrate to AIDL ClearKey DRM HAL"
    This reverts commit 57eac75437ee9af0aacf3da684fe358f6cefc297.

--- msm8996.mk
+++ msm8996.mk
@@ -192,7 +192,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.3.vendor
 
 # For config.fs
 PRODUCT_PACKAGES += \
