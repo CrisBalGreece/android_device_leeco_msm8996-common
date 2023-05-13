9c03f947b5f1fd5bbb7651b3968fc61c7afd12ed
CrisBal <crisbaladim@yahoo.gr>
2023-05-13 10:21:35 +0300

    msm8996-common: BT aidl fixes

--- msm8996.mk
+++ msm8996.mk
@@ -127,13 +127,9 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
-    libbt-vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
