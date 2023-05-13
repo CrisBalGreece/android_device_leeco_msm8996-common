551e88ffeb0b22b56576ac0f5e95392e7925c9ec
CrisBal <crisbaladim@yahoo.gr>
2023-05-13 08:27:47 +0300

    msm8996-common: Fix BT manifest and build bt-1.1.vendor
    I hwservicemanager: getTransport: Cannot find entry android.hardware.bluetooth@1.1::IBluetoothHci/default in either framework or device VINTF manifest.

--- msm8996.mk
+++ msm8996.mk
@@ -128,6 +128,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0-impl \
