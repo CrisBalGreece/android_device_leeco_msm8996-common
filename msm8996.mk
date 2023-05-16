c128c1056fc9b1ab09b2c043ae33b869045f4e1c
CrisBal <crisbaladim@yahoo.gr>
2023-05-16 08:49:51 +0300

    msm8996-common: Switch to perf@1.0.vendor
    * We are legacy

--- msm8996.mk
+++ msm8996.mk
@@ -335,7 +335,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@1.0.vendor \
+    vendor.qti.hardware.perf@1.0
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
