5caea7d4d86388a28cc19a08754dd94549342e7e
CrisBalGreece <crisbaladim@yahoo.gr>
2024-03-16 08:06:22 +0200

    msm8996-common:Drop prebuilt v32-libhardware_legacy.so

--- msm8996.mk
+++ msm8996.mk
@@ -488,8 +488,7 @@ PRODUCT_PACKAGES += \
 
 # Prebuilts VNDK
 PRODUCT_COPY_FILES += \
-prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so \
-prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libhardware_legacy.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libhardware_legacy.so
+prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so
 
 # WiFi
 PRODUCT_PACKAGES += \
