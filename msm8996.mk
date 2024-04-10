ac7dcdba5489484fb652da47954ca9f9ffda5b6d
CrisBalGreece <crisbaladim@yahoo.gr>
2024-04-10 13:14:10 +0300

    msm8996-common:Get unlimited storage for Gphotos by default

--- msm8996.mk
+++ msm8996.mk
@@ -204,6 +204,12 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v32.so
 
+# Unlimited Gphotos for Vanilla build with MicroG
+ifneq ($(LEGION_GAPPS), true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/nexus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/nexus.xml
+endif
+
 # Doze mode
 PRODUCT_PACKAGES += \
     LeEcoDoze
