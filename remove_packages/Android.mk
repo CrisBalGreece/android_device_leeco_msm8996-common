3a4195098e14e81e756407ef03e06472fe669e54
CrisBalGreece <crisbaladim@yahoo.gr>
2024-06-21 09:22:52 +0300

    msm8996-common:Update changelog and configs

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube YoutubeMusicPrebuilt Maps SafetyHubPrebuilt Drive PrebuiltGmail
+LOCAL_OVERRIDES_PACKAGES := YouTube YoutubeMusicPrebuilt Maps SafetyHubPrebuilt Drive PrebuiltGmail SoundAmplifierPrebuilt CalendarGooglePrebuilt Photos ScribePrebuilt RecorderPrebuilt PrebuiltDeskClockGoogle CalculatorGooglePrebuilt Symphonica
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
